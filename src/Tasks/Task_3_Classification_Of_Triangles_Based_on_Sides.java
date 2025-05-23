6131c228c90f6075c0dbfaeb592e135ccf920f20
GodSince1991 <jagannath.kambali@gmail.com>
2025-05-23 20:23:02 +0530

    11th May Class

---
+++ src/Tasks/Task_3_Classification_Of_Triangles_Based_on_Sides.java
@@ -0,0 +1,40 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task_3_Classification_Of_Triangles_Based_on_Sides {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Take input for sides
+        System.out.print("Enter side A: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter side B: ");
+        int b = scanner.nextInt();
+
+        System.out.print("Enter side C: ");
+        int c = scanner.nextInt();
+
+        // Check for triangle validity using the triangle inequality theorem
+        if (a + b > c && a + c > b && b + c > a) {
+            // Check for Equilateral
+            if (a == b && b == c) {
+                System.out.println("The triangle is Equilateral.");
+            }
+            // Check for Isosceles
+            else if (a == b || b == c || a == c) {
+                System.out.println("The triangle is Isosceles.");
+            }
+            // If none are equal
+            else {
+                System.out.println("The triangle is Scalene.");
+            }
+        } else {
+            System.out.println("The given sides do not form a valid triangle.");
+        }
+
+        scanner.close();
+
+    }
+}
